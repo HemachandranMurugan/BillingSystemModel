79df11bb3f974dd65776683fc002044e54a792c0
HemachandranMurugan <hemanthmurugan261@gmail.com>
2024-07-19 08:22:23 +0530

    Update Bill.java

--- ZohoProject/Bill.java
+++ ZohoProject/Bill.java
@@ -7,65 +7,46 @@ public class Bill {
     private double billamt;
     private double discount;
     private double totalAmount;
+    private boolean paidStatus;
 
-    public Bill() {
-    }
+    public Bill() {}
 
     // Getters and Setters
     public String getBillNo() {
         return billNo;
     }
-
     public void setBillNo(String billNo){
         this.billNo=billNo;
     }
-
     public double getDiscount(){
         return discount;
     }
-
     public void setDiscount(double discount){
         this.discount=discount;
     }
     public String getDate() {
-        return
-                billDate;
+        return billDate;
     }
-
-
+    
     public void setDate(String billDate){
-
         this.billDate=billDate;
     }
     public String getCustomerid() {
-
         return customerid;
     }
     public void setCustomerid(String customerid){
-
         this.customerid=customerid;
     }
-
-
     public double getbillamt() {
-
         return billamt;
     }
-
-
     public void setbillamt(double billamt) {
-
         this.billamt = billamt;
     }
-
-    public void setTotalAmt(double totalAmount) {
-
-        this.totalAmount=totalAmount;
+    public boolean getpaidStatus(){
+        return paidStatus;
     }
-
-    public double getTotalAmt() {
-
-        return totalAmount;
+    public void setPaidStatus(boolean paidStatus){
+        this.paidStatus = paidStatus;
     }
 }
-
